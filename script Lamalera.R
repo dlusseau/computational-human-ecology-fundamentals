3b272bc7ba194d1b3bed0dc85e41607178ea8c75
David Lusseau <davlu@dtu.dk>
2026-03-10 19:33:21 +0100

    initialisation of script

---
+++ script Lamalera.R
@@ -0,0 +1,15 @@
+##############################################
+##### basic run through of online sampling in 
+##### relation to a destination
+##### 10 Mar 2026
+##### let's use Lembata  Island first
+############################################
+
+library(wikipediar)
+library(sf)
+library(ggplot2)
+library(osmdata)
+
+village<-"Lamarela"
+island<-"Lembata"
+
